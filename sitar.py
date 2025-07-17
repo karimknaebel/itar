83b3de1f487ea84f9485a788c4c735666b9aa9ae
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2025-07-17 16:10:27 +0200

    don't autocreate sitar

--- sitar.py
+++ sitar.py
@@ -1,7 +1,5 @@
 import os
-from contextlib import nullcontext
 from pathlib import Path
-from typing import IO
 
 import msgpack
 from mini.itar.indexed_tar import IndexedTar, TarIndex
@@ -9,9 +7,9 @@ from mini.itar.sharded_indexed_tar import ShardedIndexedTar
 
 
 class SITar(ShardedIndexedTar):
-    def __init__(self, path: str | os.PathLike, fileobj: IO[bytes] | None = None):
+    def __init__(self, path: str | os.PathLike):
         path = Path(path)
-        with open(path, "rb") if fileobj is None else nullcontext(fileobj) as f:
+        with open(path, "rb") as f:
             shards, indices = msgpack.load(f)
         super().__init__(
             shards=[path.parent / shard for shard in shards],
@@ -23,7 +21,6 @@ class SITar(ShardedIndexedTar):
         path: str | os.PathLike,
         shards: list[str | os.PathLike],
         indices: list[TarIndex | None] | None = None,
-        fileobj: IO[bytes] | None = None,
     ):
         path = Path(path)
         if indices is None:
@@ -32,10 +29,9 @@ class SITar(ShardedIndexedTar):
             IndexedTar(tar, None).index if index is None else index
             for tar, index in zip(shards, indices, strict=True)
         ]
-        with open(path, "wb") if fileobj is None else nullcontext(fileobj) as f:
+        with open(path, "wb") as f:
             msgpack.dump(
-                ([str(Path(s).relative_to(path.parent)) for s in shards], indices),
-                f,
+                ([str(Path(s).relative_to(path.parent)) for s in shards], indices), f
             )
 
 
